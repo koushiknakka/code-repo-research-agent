b3ee1b084f65a8a623d787759e4f6f6ba30b0553
koushiknakka <nakkakiransivakoushik4232@gmail.com>
2026-05-29 11:59:10 +0530

    added vecDB

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,7 +1,8 @@
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.embeddings.fastembed import FastEmbedEmbedding
 from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex, StorageContext
+from qdrant_client import QdrantClient
 
 
 from data_parser import parse_files
@@ -12,22 +13,36 @@ load_dotenv()
 
 api_key = os.getenv("GOOGLE_API_KEY")
 
+client = QdrantClient(location=":memory:")
+
 embedding_model = GoogleGenAIEmbedding(
     model_name = "gemini-embedding-2",
     api_key=api_key,
     embed_batch_size = 100,
     )
 
-class Embeddings:
+Settings.embed_model = embedding_model
+
+vector_store = QdrantVectorStore(
+    collection_name="codebase_embeddings",
+    client=client,
+    prefer_grpc=True,
+)
+
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+class get_embeddings:
     def __init__(self, file_path):
         self.file_path = file_path
 
     def get_embeddings(self):
         nodes = parse_files(file_path=self.file_path)
-        embeddings = []
-        for i in range(len(nodes)):
-            node_embedding = embedding_model.get_text_embedding(nodes[i].text)
-            embeddings.append(node_embedding)
-        return embeddings
 
+        index = VectorStoreIndex(nodes, storage_context=storage_context)
+
+
+if __name__ == "__main__":
+    embedder = get_embeddings(file_path=Path("data/sample.md"))
+    embedder.get_embeddings()
+    print("🚀 Optimization Check: Vector Index successfully computed and stored in Qdrant!")
 
